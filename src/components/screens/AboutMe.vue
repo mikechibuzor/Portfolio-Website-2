103564ca8c78469292203de34dcd76d14f404429
mikechibuzor <iwuagwuchibuzor@gmail.com>
2021-09-09 23:53:21 +0100

    worked on the about me page/screen

--- src/components/screens/AboutMe.vue
+++ src/components/screens/AboutMe.vue
@@ -1,15 +1,44 @@
 <template>
   <div class="wrapper xl:h-height90 w-screen">
     <main class="h-full w-full pt-5 ">
-      <div class="border-decoration border-2 border-gray-500 px-28 pt-12 h-full w-full rounded">
+      <div class="border-decoration border-2 bg-gray-50 dark:bg-gray-700 border-gray-100 dark:border-gray-400 px-28 pt-12 h-full w-full rounded">
         <div class="first-half">
-          <div class="profile-photo">
-            <div class="photo-frame"></div>
-            <p>Creative Writer</p>
+          <div class="profile-photo w-36 items-center flex flex-col">
+            <div class="photo-frame border relative border-midNightBlue h-height20 w-full"></div>
+            <p class="text-xs mt-1">Creative Writer</p>
           </div>
         </div>
-        <div class="second-half"></div>
+        <div class="second-half  mt-10">
+          <p class="text-justify about-me-text text-lg dark:text-gray-300">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus ipsum amet quibusdam libero, vel dicta dolorum atque praesentium tempore reprehenderit, error exercitationem fugit expedita sequi hic at voluptates odit sapiente!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus ipsum amet quibusdam libero, vel dicta dolorum atque praesentium tempore reprehenderit, error exercitationem fugit expedita sequi hic at voluptates odit sapiente!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit adipisicing eli adipisicing eli adipisicing eli. amet consectetur, adipisicing elit... 
+            <span class="inline-block">
+               
+                <svg class="h-5 w-5" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.94801 41.2001L4.52189 38.9081L2.93936 38.2833L1.85233 40.0748C1.85233 40.0748 2.45165 41.7004 1.78601 42.9999C1.78653 43 2.61946 41.5813 3.94801 41.2001ZM2.88485 40.1484C3.07965 40.0685 3.30417 40.1596 3.38464 40.3509C3.46575 40.5422 3.37335 40.7625 3.17855 40.8423C2.98375 40.9221 2.75923 40.831 2.67876 40.6397C2.59687 40.4476 2.68941 40.2274 2.88485 40.1484Z" fill="#010002"/>
+                <path d="M20.551 14.6159C15.9938 18.3535 10.0277 26.0526 4.96799 35.0462L4.96514 35.0427L4.08069 36.6034L5.0238 36.9663L5.52215 35.6533C8.22144 30.7877 15.5443 18.0811 28.934 9.63885C28.934 9.63885 33.919 6.66988 38.8305 2.56378C37.6347 3.56334 35.2757 4.81624 33.9966 5.6245C30.6394 7.74562 26.4966 9.73904 20.551 14.6159Z" fill="#010002"/>
+                <path d="M4.3086 27.95C3.27231 31.6127 3.33161 34.4024 3.73263 35.169H4.35947C6.09034 31.5838 14.4267 18.597 19.6156 14.5319C24.9916 10.3199 29.1385 7.86918 32.6264 6.08186C33.8511 5.45438 35.1075 4.60445 36.2405 3.75245C37.6163 2.66749 39.011 1.507 40.2322 0.371299L36.6922 2.69293L38.2706 0.278767L35.2571 3.34299L37.2181 0C32.579 0.418539 28.274 2.73952 28.274 2.73952C26.5516 4.78238 25.1646 8.03219 25.1646 8.03219C25.1165 6.40722 26.2171 3.9466 26.2171 3.9466C25.4039 4.3179 22.3142 6.08264 22.3142 6.08264C20.8029 7.33631 20.3335 10.6327 20.3335 10.6327C20.0462 9.33245 21.0507 6.82589 21.0507 6.82589C16.6975 9.33323 13.5881 13.6044 13.5881 13.6044C11.4358 17.8291 13.2055 21.1255 12.5356 20.3363C11.8657 19.5471 12.0097 16.6686 12.0097 16.6686C7.46523 21.0331 6.98738 23.1218 6.98738 23.1218C6.2222 24.9789 5.55227 29.9469 5.55227 29.9469C5.12166 29.4824 5.55227 26.8827 5.55227 26.8827C4.45187 29.2043 4.97839 31.3862 4.97839 31.3862C4.45252 31.1999 4.3086 27.95 4.3086 27.95Z" fill="#010002"/>
+                <path d="M40.233 1.84167C40.233 1.84167 38.4832 3.53724 36.2412 5.22282C32.7267 7.99478 29.3427 10.2607 29.3427 10.2607C16.0856 18.0945 8.6985 30.8787 6.25696 35.2621H7.55501C8.01378 34.8943 8.70278 34.617 11.58 33.7841C15.2153 32.7316 17.3837 30.5343 17.3837 30.5343C17.3837 30.5343 14.0675 32.3914 12.5047 32.2058C10.9419 32.0202 8.84191 32.6081 8.84191 32.6081C8.84191 32.6081 8.04 32.2368 11.9944 31.6488C15.9488 31.0608 22.4541 26.6349 22.4541 26.6349C22.4541 26.6349 19.9666 27.6563 18.8188 27.5631C17.6711 27.4699 14.8328 27.5631 14.8328 27.5631C16.7139 27.1296 18.468 27.3775 21.3381 26.294C24.2082 25.2104 24.0487 25.3036 24.0487 25.3036C27.3014 24.2201 28.6525 20.8467 28.6525 20.8467C28.0899 21.0635 23.7932 21.5893 22.0109 21.0635C20.2285 20.5376 17.1919 21.0635 17.1919 21.0635C20.062 20.1041 20.7001 20.5376 21.4011 20.5066C22.1027 20.4756 24.3672 20.6611 28.8636 19.1139C33.3601 17.5667 34.6356 15.6474 34.6356 15.6474L31.6032 16.0525C33.5832 15.7194 35.8152 14.4093 35.8152 14.4093C34.4436 14.7806 32.307 14.9662 32.307 14.9662C34.8262 14.4093 36.2295 13.6355 36.2295 13.6355C38.5893 11.8404 39.1314 9.17863 39.1314 9.17863C35.7827 10.3235 33.1364 9.79768 33.1364 9.79768C37.7289 9.48855 39.0044 8.31184 39.6417 8.03372C40.2798 7.75495 40.5424 6.60225 40.5424 6.60225C38.2222 7.43803 35.5913 7.57702 35.5913 7.57702C38.4374 7.1359 40.8762 5.69652 40.8762 5.69652V4.2557C39.7758 4.88254 37.8395 5.37025 37.8395 5.37025L40.8528 3.42057C41.0222 2.44515 40.233 1.84167 40.233 1.84167Z" fill="#010002"/>
+                <path d="M3.0564 37.7102L4.84865 38.4281L5.18245 37.6347L3.39032 36.9168L3.0564 37.7102Z" fill="#010002"/>
+                <path d="M22.9756 37.0466C17.054 39.8935 11.3653 41.8339 3.79964 42.0881C3.09377 42.1121 3.09026 42.8179 3.79964 42.7939C10.5676 42.5667 16.1992 41.1119 21.6302 38.5664C27.6803 35.7301 33.237 33.3535 40.8152 32.5459C41.5141 32.471 41.2169 31.7913 40.523 31.8654C33.8116 32.5813 28.3586 34.459 22.9756 37.0466Z" fill="#010002"/>
+                </svg>
+
+            </span>
+          </p>
+        </div>
+         <div class="bottom mt-10 h-height10 w-full flex justify-end ">
+           <div>
+             <h3 class="text-2xl font-bold dark:text-gray-300">Certifications</h3>
+             <ul class="text-sm mt-1 text-gray-700 dark:text-gray-300">
+               <li>Lorem ipsum dolor sit amet, consectetur adipisicing.</li>
+               <li>Lorem ipsum dolor sit amet, consectetur adipisicing.</li>
+               <li>Lorem ipsum dolor sit amet, consectetur adipisicing.</li>
+             </ul>
+           </div>
+         </div>
       </div>
+     
     </main>
   </div>
 </template>
@@ -21,5 +50,36 @@ export default {
 </script>
 
 <style scoped>
-  
+/* @import url('https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap'); */
+/* @import url('https://fonts.googleapis.com/css2?family=Indie+Flower&family=Sacramento&display=swap'); */
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap');
+
+.about-me-text{
+  /* font-family: 'Indie Flower', cursive; */
+  /* font-family: 'Sacramento', cursive; */
+  font-family: 'Cormorant Garamond', serif;
+  font-style: italic;
+  font-weight: 600;
+  /* opacity: .9; */
+}
+h3{
+  font-family: 'Dancing Script', cursive;
+}
+  .photo-frame::after{
+    content: "";
+    position: absolute;
+    height: 105%;
+    width: 105%;
+    border: 1px solid rgb(25, 25, 112);
+    left: 50%;
+    top: 50%;
+    transform: translate(-50% ,-50%);
+  }
+
+    html.dark svg path{
+    /* stroke: #FBBF24; */
+    /* fill: #FBBF24; */
+    fill: rgba(209, 213, 219, 1);
+  }
 </style>
